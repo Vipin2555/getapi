dc395c07ce3bb56ddbe69edd43480312c846b5ee
Vipin2555 <vipinmandloi43@gmail.com>
2023-10-17 13:06:19 +0530

    cors

--- api.js
+++ api.js
@@ -1,7 +1,9 @@
 const express=require("express")
 const app=express()
+const cors=require("cors")
 require("./mongoose")
 const student=require("./schema")
+app.use(cors())
 app.get("/",async (req,resp)=>
 {
     const data=await student.find()
